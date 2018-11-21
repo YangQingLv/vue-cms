2e8a8fce133ce15344897f301ed9c502d2386761
LYQ <987176285@qq.com>
2018-11-21 12:07:16 +0800

    完成头部固定栏

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="app">
-    
+    <h1>这是 APP 组件</h1>
+    <!-- 顶部 Header 区 -->
+    <mt-header fixed title="固定在顶部"></mt-header>
+    <!-- 底部 Tabbar 区 -->
+    <!-- 中间 router-view 区 -->
+    <mt-header fixed title="固定在顶部"></mt-header>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// 入口文件
 import Vue from 'vue'
+
+// 1. 导入 vue-router 路由 包 
+import VueRouter from 'vue-router'
+// 2. 手动安装路由
+Vue.use(VueRouter)
+  
+// 按需导入 Mint-UI 中的组件
+import { Header } from 'mint-ui';
+
+Vue.component(Header.name, Header);
+
+// 导入App 跟组件
 import App from './App'
+
+// 导入自己的 router.js 路由模块
 import router from './router'
 
 Vue.config.productionTip = false
@@ -9,6 +22,7 @@ Vue.config.productionTip = false
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  router: c => c(app),
   router,
   components: { App },
   template: '<App/>'
